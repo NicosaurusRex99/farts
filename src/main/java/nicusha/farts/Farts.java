a7960bc64aba9d2e854e077f67e94aecb1083dfa
Nicusha <nicosaurusrex99@gmail.com>
2025-04-16 15:16:52 +0930

    port

--- src/main/java/nicusha/farts/Farts.java
+++ src/main/java/nicusha/farts/Farts.java
@@ -1,17 +1,17 @@
 package nicusha.farts;
 
-import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import nicusha.farts.registry.SoundRegistry;
-import org.slf4j.Logger;
+
+import java.util.logging.Logger;
 
 @Mod(Farts.MODID)
 public class Farts
 {
     public static final String MODID = "farts";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = Logger.getLogger(MODID);
 
     public Farts(IEventBus bus, ModContainer mod)
     {
